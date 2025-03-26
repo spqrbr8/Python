bc06abc59d807eb56f70b19c2f05b4cd80be43d3
Stefan <untilastefan8@gmail.com>
2025-03-26 16:36:15 -0400

    addded option

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+def get_argument():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--ip", dest="ip", help="Add IP address")
+    options = parser.parse_args()
+    if not options.ip:
+        parser.error("[-] Please specify an IP address, use --help for more info")
+    return options.ip
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
@@ -8,12 +17,18 @@ def scan(ip):
     arp_request_broadcast = broadcast/arp_request
     answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
-    print("IP\t\t\tMAC Address\n------------------------------------------")
+
     clients_list = []
     for element in answered_list:
         client_dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
         clients_list.append(client_dict)
-        print(element[1].psrc + "\t\t" + element[1].hwsrc)
-    print(clients_list)
+    return clients_list
 
-scan("10.0.2.1/24")
+def print_result(results_list):
+    print("IP\t\t\tMAC Address\n------------------------------------------")
+    for client in results_list:
+        print(client["ip"] + "\t\t" + client["mac"])
+
+ip = get_argument()
+scan_result = scan(ip)
+print_result(scan_result)
